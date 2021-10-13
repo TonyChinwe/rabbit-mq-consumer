5050743c24c911e70e1107c969f3b86fd19171b0
Anthony Chinwe <chinwe221@gmail.com>
2021-10-13 05:11:32 -0700

    first commit

---
+++ src/test/java/com/phisoft/rabbitmqconsumer/RabbitMqConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phisoft.rabbitmqconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
